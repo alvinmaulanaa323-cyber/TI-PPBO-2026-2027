31be5a1c313c00dac66117ce879aa30189c90065
Alvin Maulana <alvinmaulanaa323@gmail.com>
2026-09-07 22:05:51 +0700

    A.2. Struktur Dasar Program Java

--- praktikum-2/src/HelloWorld.java
+++ praktikum-2/src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Alvin maulana");
+
+        System.out.print("Nama: ");
+        System.out.print("Budi");
+        System.out.println();
+        System.out.println("Ini baris baru");
     }
 }
