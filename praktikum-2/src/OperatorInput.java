c123c44fba7cb893c9c0c46861fe372155eb3f33
Alvin Maulana <alvinmaulanaa323@gmail.com>
2026-09-09 23:49:08 +0700

    Latihan 5 operatorInput

---
+++ praktikum-2/src/OperatorInput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class OperatorInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan bilangan pertama: ");
+        int a = sc.nextInt();
+
+        System.out.print("Masukkan bilangan kedua: ");
+        int b = sc.nextInt();
+
+        System.out.println("Penjumlahan: " + (a + b));
+        System.out.println("Pengurangan: " + (a - b));
+        System.out.println("Perkalian: " + (a * b));
+        System.out.println("Pembagian: " + (a / b));
+        System.out.println("Sisa bagi: " + (a % b));
+
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a == b: " + (a == b));
+    }
+}
