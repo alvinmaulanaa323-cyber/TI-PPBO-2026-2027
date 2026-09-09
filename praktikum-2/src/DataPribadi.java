67d19a7ce74a72e94a0c9eb0a2f89ffc91c7b062
Alvin Maulana <alvinmaulanaa323@gmail.com>
2026-09-09 23:28:52 +0700

    Latihan 3 Data Pribadi

---
+++ praktikum-2/src/DataPribadi.java
@@ -0,0 +1,20 @@
+public class DataPribadi {
+    public static void main(String[] args) {
+        // Variabel untuk menyimpan umur
+        int umur = 20;
+
+        // Variabel untuk menyimpan tinggi badan
+        double tinggiBadan = 168;
+
+        // Variabel untuk menyimpan inisial
+        char inisial = 'A';
+
+        // Variabel untuk menyimpan status mahasiswa aktif
+        boolean mahasiswaAktif = true;
+
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi Badan: " + tinggiBadan);
+        System.out.println("Inisial: " + inisial);
+        System.out.println("Mahasiswa Aktif: " + mahasiswaAktif);
+    }
+}
