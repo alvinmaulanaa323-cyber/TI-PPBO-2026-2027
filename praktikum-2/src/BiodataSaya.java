5db2edb35e965fdf26cbf3ba4c2689a3675fbec0
Alvin Maulana <alvinmaulanaa323@gmail.com>
2026-09-09 23:05:41 +0700

    Latihan 1 sebelum di modifikasi

---
+++ praktikum-2/src/BiodataSaya.java
@@ -0,0 +1,11 @@
+public class BiodataSaya {
+    public static void main(String[] args) {
+
+        // Menampilkan biodata mahasiswa
+        System.out.println("Nama: Alvin maulana");
+        System.out.println("NIM: 2025573010003");
+
+        // Menampilkan program studi
+        System.out.println("Program Studi: Teknik Informatika");
+    }
+}
